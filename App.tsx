c397767d6c88dd60cfe743935066b011bcc6cb7d
elmano777 <rolandodiazjara@gmail.com>
2024-07-29 13:59:39 -0500

    try_form

--- App.tsx
+++ App.tsx
@@ -1,11 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Button, Image, Text, TextInput, View } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 export default function App() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+  const onSubmit = (data: any) => console.log(data);
   return (
-    <View className="items-center justify-center flex-1 bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-col items-center justify-center flex-1 space-x-4">
+      <View className="flex flex-row items-center space-x-4">
+        <StatusBar style="auto" />
+        <Image
+          source={require("./assets/images/65671cd627825dadd6139e7b_analytics-color.webp")}
+          className="w-24 h-24"
+        />
+        <View className="">
+          <Text>Chayan</Text>
+          <Text>Technology</Text>
+        </View>
+      </View>
+      <View className="flex flex-col mt-5">
+        <Controller
+          control={control}
+          rules={{
+            required: true,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="First name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="firstName"
+        />
+        {errors.firstName && <Text>This is required.</Text>}
+        <Controller
+          control={control}
+          rules={{
+            maxLength: 100,
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Last name"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+          name="lastName"
+        />
+        <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+      </View>
     </View>
   );
 }
